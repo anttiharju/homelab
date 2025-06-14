56cb5a798f5aaaf88631e3fc0eb458d327784570
Antti Harju <antti@harju.io>
2025-06-14 16:09:30 +0300

    Reject Talos and Kairos as solutions

--- talos/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd /tmp
-version=1.10.3
-url="https://github.com/siderolabs/talos/releases/download/v$version/metal-amd64.iso"
-curl -LsSf -o talos-amd64.iso "$url"
-sudo dd if=talos-amd64.iso of=/dev/nvme0n1 bs=4M status=progress
-sudo reboot now
